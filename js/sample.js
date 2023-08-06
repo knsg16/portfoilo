2af2dcd1e7e83ed6f03c9e10afe8b982dbce463d
YuukiKanasugi <knsg16@gmail.com>
2023-08-06 16:30:13 +0900

    update

--- js/sample.js
+++ js/sample.js
@@ -21,39 +21,30 @@ $(function(){
 
 
   $('#introductionTsx').on('click', function(e) {
-    // $('p').css('color','blue');
     $('#introductionTsCode').hide();
     $('#introductionTsxCode').show();
 
-    $('#introductionTsx').css('background-color','#4B4F53');
     $('#introductionTsx').css('border-left','solid 3px #8BE9FD');
 
     $('#introductionTs').css('border-left','solid 3px #313437');
     $('#introductionTs').css('background-color','#313437');
 
-    $('#introductionTsxTab').css('background-color','#4B4F53');
     $('#introductionTsxTab').css('border-bottom','solid 3px #8BE9FD');
 
-    $('#introductionTsTab').css('background-color','#282A36');
     $('#introductionTsTab').css('border-bottom','solid 3px #282A36');
-
   });
 
   $('#introductionTs').on('click', function(e) {
-    // $('p').css('color','red');
     $('#introductionTsxCode').hide();
     $('#introductionTsCode').show();
 
-    $('#introductionTs').css('background-color','#4B4F53');
     $('#introductionTs').css('border-left','solid 3px #8BE9FD');
 
     $('#introductionTsx').css('border-left','solid 3px #313437');
     $('#introductionTsx').css('background-color','#313437');
 
-    $('#introductionTsxTab').css('background-color','#282A36');
     $('#introductionTsxTab').css('border-bottom','solid 3px #282A36');
 
-    $('#introductionTsTab').css('background-color','#4B4F53');
     $('#introductionTsTab').css('border-bottom','solid 3px #8BE9FD');
   });
 
@@ -62,16 +53,13 @@ $(function(){
     $('#introductionTsxCode').hide();
     $('#introductionTsCode').show();
 
-    $('#introductionTs').css('background-color','#4B4F53');
     $('#introductionTs').css('border-left','solid 3px #8BE9FD');
 
     $('#introductionTsx').css('border-left','solid 3px #313437');
     $('#introductionTsx').css('background-color','#313437');
 
-    $('#introductionTsxTab').css('background-color','#282A36');
     $('#introductionTsxTab').css('border-bottom','solid 3px #282A36');
 
-    $('#introductionTsTab').css('background-color','#4B4F53');
     $('#introductionTsTab').css('border-bottom','solid 3px #8BE9FD');
   });
 
@@ -80,16 +68,13 @@ $(function(){
     $('#introductionTsCode').hide();
     $('#introductionTsxCode').show();
 
-    $('#introductionTsx').css('background-color','#4B4F53');
     $('#introductionTsx').css('border-left','solid 3px #8BE9FD');
 
     $('#introductionTs').css('border-left','solid 3px #313437');
     $('#introductionTs').css('background-color','#313437');
 
-    $('#introductionTsxTab').css('background-color','#4B4F53');
     $('#introductionTsxTab').css('border-bottom','solid 3px #8BE9FD');
 
-    $('#introductionTsTab').css('background-color','#282A36');
     $('#introductionTsTab').css('border-bottom','solid 3px #282A36');
 
   });
